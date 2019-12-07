1a751c493d658df25ca5f5b6b42b318e57e35c28
James Kerrane <hi@thatrobot.dev>
2019-12-07 23:25:14 +0000

    Finished user script

---
+++ account.js
@@ -0,0 +1,18 @@
+function loadSelect() {
+  window.location.href = "/loadselect.html";
+}
+
+function frog() {
+  localStorage.user = "frog";
+  loadSelect()
+}
+
+function tiger() {
+  localStorage.user = "tiger";
+  loadSelect()
+}
+
+function penguin() {
+  localStorage.user = "penguin";
+  loadSelect()
+}
--- script.js
+++
@@ -1,3 +0,0 @@
-var user = "Tiger";
-localStorage.user = user;
-
